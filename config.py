a13b26fd1a119e7af9b052f343a3ba6803b9b1a8
Jonny Rose <jonny_rose44@yahoo.com>
2021-10-09 13:59:44 +0300

    9th lesson: Pagination

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['jonny_rose44@yahoo.com']
+    
+    POSTS_PER_PAGE = 25
